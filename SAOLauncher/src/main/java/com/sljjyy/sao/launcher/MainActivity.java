58246acd868b75c4a65432800214f93d8351535f
XiaoYe <sljjyy@hotmail.com>
2013-09-23 17:20:36 +0800

    Load App List

--- SAOLauncher/src/main/java/com/sljjyy/sao/launcher/MainActivity.java
+++ SAOLauncher/src/main/java/com/sljjyy/sao/launcher/MainActivity.java
@@ -1,15 +1,29 @@
 package com.sljjyy.sao.launcher;
 
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
+    GridView appsGrid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadApps();
+        appsGrid = (GridView) findViewById(R.id.apps_list);
+        appsGrid.setAdapter(new AppsAdapter());
     }
 
 
@@ -19,5 +33,51 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+    private List<ResolveInfo> apps;
+    private void loadApps() {
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        new ImageView(MainActivity.this);
+
+        apps = getPackageManager().queryIntentActivities(mainIntent, 0);
+    }
+
+
+    public class AppsAdapter extends BaseAdapter {
+
+        public AppsAdapter(){
+        }
+
+        @Override
+        public int getCount() {
+            return apps.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return apps.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ImageView iv;
+
+            if(view == null){
+                iv = new ImageView(MainActivity.this);
+                iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                iv.setLayoutParams(new GridView.LayoutParams(50, 50));
+            } else {
+                iv = (ImageView) view;
+            }
+            ResolveInfo info = apps.get(i);
+            iv.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));
+            return iv;
+        }
+    }
 }
