3dca11dd9c040c3c1e4e39c13ce2026ef0d0d401
XiaoYe <sljjyy@hotmail.com>
2013-09-23 17:20:36 +0800

    Menu Coding Test

--- SAOLauncher/src/main/java/com/sljjyy/sao/launcher/MainActivity.java
+++ SAOLauncher/src/main/java/com/sljjyy/sao/launcher/MainActivity.java
@@ -1,23 +1,125 @@
 package com.sljjyy.sao.launcher;
 
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.AdapterView.OnItemClickListener;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
+    GridView appsGrid;
+    private List<ResolveInfo> apps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadApps();
+        appsGrid = (GridView) findViewById(R.id.apps_list);
+        appsGrid.setAdapter(new AppsAdapter());
+        appsGrid.setOnItemClickListener(clickListener);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+        public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                onSetWallpaper();
+                return true;
+        }
+        return false;
+    }
+
+
+    private OnItemClickListener clickListener = new OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            ResolveInfo info = apps.get(i);
+            //该应用的包名
+            String pkg = info.activityInfo.packageName;
+            //应用的主activity类
+            String cls = info.activityInfo.name;
+            ComponentName componet = new ComponentName(pkg, cls);
+
+            Intent intent = new Intent();
+            intent.setComponent(componet);
+            startActivity(intent);
+        }
+    };
+
+    public void onSetWallpaper() {
+        //生成一个设置壁纸的请求
+        final Intent pickWallpaper = new Intent(Intent.ACTION_SET_WALLPAPER);
+        Intent chooser = Intent.createChooser(pickWallpaper,"chooser_wallpaper");
+        //发送设置壁纸的请求
+        startActivity(chooser);
+    }
+
+    private void loadApps() {
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        new ImageView(MainActivity.this);
+
+        apps = getPackageManager().queryIntentActivities(mainIntent, 0);
+    }
+
+
+    public class AppsAdapter extends BaseAdapter {
+
+        public AppsAdapter(){
+        }
+
+        @Override
+        public int getCount() {
+            return apps.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return apps.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ImageView iv;
+
+            if(view == null){
+                iv = new ImageView(MainActivity.this);
+                iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                iv.setLayoutParams(new GridView.LayoutParams(50, 50));
+            } else {
+                iv = (ImageView) view;
+            }
+            ResolveInfo info = apps.get(i);
+            iv.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));
+            return iv;
+        }
+    }
 }
