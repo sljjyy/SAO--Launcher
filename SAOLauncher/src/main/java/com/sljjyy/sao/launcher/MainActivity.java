87ed0f15d74ca5cbe162afd5dcc81f6a6b99728a
XiaoYe <sljjyy@hotmail.com>
2013-09-30 16:01:31 +0800

    DB Activity

--- SAOLauncher/src/main/java/com/sljjyy/sao/launcher/MainActivity.java
+++ SAOLauncher/src/main/java/com/sljjyy/sao/launcher/MainActivity.java
@@ -1,39 +1,28 @@
 package com.sljjyy.sao.launcher;
 
-import android.content.ComponentName;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SubMenu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
-import java.util.List;
+import com.sljjyy.sao.launcher.Controller.DatabaseHelper;
+
 
 public class MainActivity extends Activity {
-    GridView appsGrid;
-    private List<ResolveInfo> apps;
+
+    ListView oneMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadApps();
-        appsGrid = (GridView) findViewById(R.id.apps_list);
-        appsGrid.setAdapter(new AppsAdapter());
-        appsGrid.setOnItemClickListener(clickListener);
-    }
+        oneMenu = (ListView) findViewById(R.id.one_menu);
+        final DatabaseHelper helpter = new DatabaseHelper(this);
 
+    }
 
     @Override
         public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,23 +40,6 @@ public class MainActivity extends Activity {
         return false;
     }
 
-
-    private OnItemClickListener clickListener = new OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            ResolveInfo info = apps.get(i);
-            //该应用的包名
-            String pkg = info.activityInfo.packageName;
-            //应用的主activity类
-            String cls = info.activityInfo.name;
-            ComponentName componet = new ComponentName(pkg, cls);
-
-            Intent intent = new Intent();
-            intent.setComponent(componet);
-            startActivity(intent);
-        }
-    };
-
     public void onSetWallpaper() {
         //生成一个设置壁纸的请求
         final Intent pickWallpaper = new Intent(Intent.ACTION_SET_WALLPAPER);
@@ -76,50 +48,8 @@ public class MainActivity extends Activity {
         startActivity(chooser);
     }
 
-    private void loadApps() {
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        new ImageView(MainActivity.this);
+    private void initDB(){
 
-        apps = getPackageManager().queryIntentActivities(mainIntent, 0);
     }
 
-
-    public class AppsAdapter extends BaseAdapter {
-
-        public AppsAdapter(){
-        }
-
-        @Override
-        public int getCount() {
-            return apps.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return apps.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            ImageView iv;
-
-            if(view == null){
-                iv = new ImageView(MainActivity.this);
-                iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                iv.setLayoutParams(new GridView.LayoutParams(50, 50));
-            } else {
-                iv = (ImageView) view;
-            }
-            ResolveInfo info = apps.get(i);
-            iv.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));
-            return iv;
-        }
-    }
 }
